24f010181e2697abc862f9e67073f59dd08a4bd0
pacifi <pacifi.bnr@gmail.com>
2019-02-25 21:20:09 -0500

    new serializer

--- rest/serializers.py
+++ rest/serializers.py
@@ -4,10 +4,19 @@ from payment.models import Payment
 
 
 class PaymentSerializer(serializers.ModelSerializer):
+    person_name = serializers.SerializerMethodField()
+    number_transaction = serializers.SerializerMethodField()
+    institution_name = serializers.SerializerMethodField()
 
     class Meta:
         model = Payment
         exclude = ()
-        depth = 1
 
+    def get_person_name(self, obj):
+        return "%s %s %s" % (obj.person.first_name, obj.person.last_father_name, obj.person.last_mother_name)
 
+    def get_number_transaction(self, obj):
+        return "%s" % (obj.id)
+
+    def get_institution_name(self, obj):
+        return "%s" % (obj.institution.name)
